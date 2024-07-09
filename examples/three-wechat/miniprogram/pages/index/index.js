455c358b1ad806141a62d6dd5fc900f222de8eed
yingbin.yang@midea.com <yingbin.yang@midea.com>
2024-07-09 14:56:44 +0800

    开始从3d复制过来

--- examples/three-wechat/miniprogram/pages/index/index.js
+++ examples/three-wechat/miniprogram/pages/index/index.js
@@ -2,7 +2,6 @@
 
 var three = require("../../chunks/three.js");
 var screenshot = require("../../chunks/screenshot.js");
-var testsThree = require("../../chunks/tests-three.js");
 
 function _optionalChain(ops) {
   let lastAccessLHS = undefined;
@@ -28,31 +27,6 @@ function _optionalChain(ops) {
 
 console.log("THREE Version", three.REVISION);
 
-const DEMO_MAP = {
-  MemoryTest: testsThree.DemoMemoryTest,
-  VSMShadow: testsThree.DemoVSMShadow,
-  VTKLoader: testsThree.DemoVTKLoader,
-  MeshOpt: testsThree.DemoMeshOpt,
-  TGALoader: testsThree.DemoTGALoader,
-  PDBLoader: testsThree.DemoPDBLoader,
-  STLLoader: testsThree.DemoSTLLoader,
-  TTFLoader: testsThree.DemoTTFLoader,
-  BVHLoader: testsThree.DemoBVHLoader,
-  FBXLoader: testsThree.DemoFBXLoader,
-  LWOLoader: testsThree.DemoLWOLoader,
-  MTLLoader: testsThree.DemoMTLLoader,
-  EXRLoader: testsThree.DemoEXRLoader,
-  OBJLoader: testsThree.DemoOBJLoader,
-  SVGLoader: testsThree.DemoSVGLoader,
-  RGBELoader: testsThree.DemoRGBELoader,
-  GLTFLoader: testsThree.DemoGLTFLoader,
-  ColladaLoader: testsThree.DemoColladaLoader,
-  MeshQuantization: testsThree.DemoMeshQuantization,
-  ThreeSpritePlayer: testsThree.DemoThreeSpritePlayer,
-  HDRPrefilterTexture: testsThree.DemoHDRPrefilterTexture,
-  DeviceOrientationControls: testsThree.DemoDeviceOrientationControls,
-};
-
 const getNode = (id, self) =>
   new Promise((r) =>
     wx
@@ -63,6 +37,7 @@ const getNode = (id, self) =>
       .exec(r)
   );
 
+var threeBehavior = require("three-behavior");
 // @ts-ignore
 Component({
   disposing: false,
@@ -72,8 +47,8 @@ Component({
   platform: null,
   helperCanvas: null,
 
+  behaviors: [threeBehavior],
   data: {
-    showMenu: true,
     showCanvas: false,
     currItem: -1,
     menuList: [
@@ -103,203 +78,86 @@ Component({
   },
 
   attached() {
-    this.onReady()
+    this.onReady();
   },
   detached() {
-    this.onUnload()
+    this.onUnload();
   },
 
-  methods:{
-  onReady() {
-    this.onCanvasReady();
-  },
+  methods: {
+    onReady() {
+      this.onCanvasReady();
+    },
 
-  onCanvasReady() {
-    console.log("onCanvasReady");
-    Promise.all([getNode("#gl",this), getNode("#canvas",this)]).then(
-      ([glRes, canvasRes]) => {
-        // @ts-ignore
-        this.initCanvas(glRes[0].node, canvasRes[0].node);
-      }
-    );
-  },
+    onCanvasReady() {
+      console.log("onCanvasReady");
+      Promise.all([getNode("#gl", this), getNode("#canvas", this)]).then(
+        ([glRes, canvasRes]) => {
+          // @ts-ignore
+          this.initCanvas(glRes[0].node, canvasRes[0].node);
+        }
+      );
+    },
 
-  initCanvas(canvas, helperCanvas) {
-    const platform = new screenshot.WechatPlatform(canvas);
-    this.platform = platform;
-    platform.enableDeviceOrientation("game");
-    three.PlatformManager.set(platform);
+    initCanvas(canvas, helperCanvas) {
+      const platform = new screenshot.WechatPlatform(canvas);
+      this.platform = platform;
+      platform.enableDeviceOrientation("game");
+      three.PlatformManager.set(platform);
 
-    console.log(
-      three.PlatformManager.polyfill.window.innerWidth,
-      three.PlatformManager.polyfill.window.innerHeight
-    );
-    console.log(canvas.width, canvas.height);
+      console.log(
+        three.PlatformManager.polyfill.window.innerWidth,
+        three.PlatformManager.polyfill.window.innerHeight
+      );
+      console.log(canvas.width, canvas.height);
 
-    const renderer = new three.WebGL1Renderer({
-      canvas,
-      antialias: true,
-      alpha: false,
-    });
-    const camera = new three.PerspectiveCamera(
-      75,
-      canvas.width / canvas.height,
-      0.1,
-      1000
-    );
-    const scene = new three.Scene();
-    const clock = new three.Clock();
-    const gltfLoader = new screenshot.GLTFLoader();
-    const textureLoader = new three.TextureLoader();
+      console.log("canvas inited");
 
-    this.deps = { renderer, camera, scene, clock, gltfLoader, textureLoader };
-    this.helperCanvas = helperCanvas;
+      this.setData({ showCanvas: true });
 
-    scene.position.z = -3;
-    scene.background = new three.Color(0xffffff);
-    renderer.outputEncoding = three.sRGBEncoding;
-    renderer.setPixelRatio(2);
-    // renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(canvas.width, canvas.height);
+      this.threeMounted(canvas);
+    },
 
-    const render = () => {
-      if (this.disposing) return;
-      three.PlatformManager.polyfill.requestAnimationFrame(render);
+    async onMenuItemClick(e) {
+      const { i, item } = e.currentTarget.dataset;
+      wx.showLoading({ mask: false, title: "加载中" });
+      if (this.switchingItem || !DEMO_MAP[item]) return;
+
       _optionalChain([
         this.currDemo,
         "optionalAccess",
-        (_) => _.update,
+        (_3) => _3.dispose,
         "call",
-        (_2) => _2(),
+        (_4) => _4(),
       ]);
-      renderer.render(scene, camera);
-    };
+      this.switchingItem = true;
+      this.currDemo = null;
 
-    render();
-    console.log("canvas inited");
+      const demo = new DEMO_MAP[item](this.deps);
+      await demo.init();
+      this.currDemo = demo;
+      this.setData({ currItem: i });
+      this.onMenuClick();
+      this.switchingItem = false;
+      wx.hideLoading();
+    },
+
+    onTX(e) {
+      this.platform.dispatchTouchEvent(e);
+    },
+
+    onUnload() {
+      this.disposing = true;
+      _optionalChain([
+        this.currDemo,
+        "optionalAccess",
+        (_5) => _5.dispose,
+        "call",
+        (_6) => _6(),
+      ]);
+      three.PlatformManager.dispose();
+    },
+
+    onShareAppMessage() {},
   },
-
-  onMenuClick() {
-    const showMenu = !this.data.showMenu;
-    if (showMenu) {
-      this.setData({ showMenu, showCanvas: false });
-    } else {
-      this.setData({ showMenu });
-      setTimeout(() => {
-        this.setData({ showCanvas: true });
-      }, 330);
-    }
-  },
-
-  async onMenuItemClick(e) {
-    const { i, item } = e.currentTarget.dataset;
-    wx.showLoading({ mask: false, title: "加载中" });
-    if (this.switchingItem || !DEMO_MAP[item]) return;
-
-    _optionalChain([
-      this.currDemo,
-      "optionalAccess",
-      (_3) => _3.dispose,
-      "call",
-      (_4) => _4(),
-    ]);
-    this.switchingItem = true;
-    this.currDemo = null;
-
-    const demo = new DEMO_MAP[item](this.deps);
-    await demo.init();
-    this.currDemo = demo;
-    this.setData({ currItem: i });
-    this.onMenuClick();
-    this.switchingItem = false;
-    wx.hideLoading();
-  },
-
-  onTX(e) {
-    this.platform.dispatchTouchEvent(e);
-  },
-
-  screenshot() {
-    const { renderer, scene, camera } = this.deps;
-    const [data, w, h] = screenshot.screenshot(
-      renderer,
-      scene,
-      camera,
-      three.WebGLRenderTarget
-    );
-    const ctx = this.helperCanvas.getContext("2d");
-    const imgData = this.helperCanvas.createImageData(data, w, h);
-    this.helperCanvas.height = imgData.height;
-    this.helperCanvas.width = imgData.width;
-    ctx.putImageData(imgData, 0, 0);
-    const imgDataFromCanvas = ctx.getImageData(0, 0, w, h);
-    const hasPixel = imgDataFromCanvas.data.some((i) => i !== 0);
-    console.log("hasPixel", hasPixel);
-    wx.canvasToTempFilePath({
-      // @ts-ignore
-      canvas: this.helperCanvas,
-      success(res) {
-        wx.previewImage({
-          urls: [res.tempFilePath],
-        });
-      },
-    });
-  },
-
-  async screenrecord() {
-    console.log("screenrecord clicked");
-    const fps = 20;
-    const canvas = this.deps.renderer.domElement;
-    const recorder = wx.createMediaRecorder(canvas, {
-      fps,
-      videoBitsPerSecond: 600,
-      duration: 5,
-    });
-
-    await new Promise((resolve) => {
-      recorder.on("start", resolve);
-      recorder.start();
-    });
-    console.log("start");
-
-    let frames = fps * 5;
-    while (frames--) {
-      await new Promise((resolve) => recorder.requestFrame(resolve));
-      await new Promise((resolve) => setTimeout(resolve, 1000 / fps));
-      console.log(frames);
-      // render()
-    }
-
-    const { tempFilePath } = await new Promise((resolve) => {
-      recorder.on("stop", resolve);
-      recorder.stop();
-    });
-    console.log(tempFilePath);
-
-    recorder.destroy();
-
-    wx.previewMedia({
-      sources: [
-        {
-          url: tempFilePath,
-          type: "video",
-        },
-      ],
-    });
-  },
-
-  onUnload() {
-    this.disposing = true;
-    _optionalChain([
-      this.currDemo,
-      "optionalAccess",
-      (_5) => _5.dispose,
-      "call",
-      (_6) => _6(),
-    ]);
-    three.PlatformManager.dispose();
-  },
-
-  onShareAppMessage() {},
-}
 });
